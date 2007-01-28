a388073270b4a36aa9d9f0a65c4f3e5d2e9dc541
Maighstir <maighstir@ops-area.net>
2007-01-28 23:28:15 +0100

    Start working on V2
    GUI seems difficult, plan to add it to 2.0, 1.x branch will stay GUI-less

--- AutomagicalLogger.lua
+++ AutomagicalLogger.lua
@@ -1,22 +1,49 @@
-function AutomagicalLogger()
---Enable/disable the logs by removing/adding the "--" in the corresponding if...else...end statements
---(By default, combat log is disabled and chat log is enabled)
------CHAT LOG----- 
-	if (LoggingChat()) then
-		DEFAULT_CHAT_FRAME:AddMessage("Chat is already being logged");
-	else
-		DEFAULT_CHAT_FRAME:AddMessage("Chat is not being logged - starting it!");  
-		LoggingChat(1);
-		DEFAULT_CHAT_FRAME:AddMessage("Chat is now being logged to Logs\\WOWChatLog.txt");
+function AutomagicalLogger_Start()
+	AutomagicalLogger_Init();
+	if (AutomagicalLoggerSettings.ChatLog==1) then
+		if (LoggingChat()) then
+			DEFAULT_CHAT_FRAME:AddMessage(AUTOLOGGER_CHAT_ALREADY_STARTED);
+		else
+			DEFAULT_CHAT_FRAME:AddMessage(AUTOLOGGER_CHAT_STARTING);  
+			LoggingChat(1);
+			DEFAULT_CHAT_FRAME:AddMessage(AUTOLOGGER_CHAT_STARTED);
+		end
 	end
------COMBAT LOG-----
---	if (LoggingCombat()) then
---		DEFAULT_CHAT_FRAME:AddMessage("Combat is already being logged");
---	else
---		DEFAULT_CHAT_FRAME:AddMessage("Combat is not being logged - starting it!");  
---		LoggingCombat(1);
---		DEFAULT_CHAT_FRAME:AddMessage("Combat is now being logged to Logs\\WOWChatLog.txt");
---	end	
-end 
+	if (AutomagicalLoggerSettings.CombatLog==1) then
+		if (LoggingCombat()) then
+			DEFAULT_CHAT_FRAME:AddMessage(AUTOLOGGER_COMBAT_ALREADY_STARTED);
+		else
+			DEFAULT_CHAT_FRAME:AddMessage(AUTOLOGGER_COMBAT_STARTING);  
+			LoggingCombat(1);
+			DEFAULT_CHAT_FRAME:AddMessage(AUTOLOGGER_COMBAT_STARTED);
+		end
+	end
+end
 
-AutomagicalLogger();
+function AutomagicalLogger_Init()
+	local AutomagicalLogger_Defaults={
+		["chatlog"]=1,
+		["combatlog"]=0,
+	}
+
+	if(AutomagicalLoggerSettings) then
+		-- Check the chat log
+		if(not AutomagicalLoggerSettings.ChatLog) then
+			AutomagicalLoggerSettings.ChatLog = AUTOLOGGER_DEFAULT_CHAT;
+		end
+
+		-- Check the combat log
+		if(not AutomagicalLoggerSettings.CombatLog) then
+			AutomagicalLoggerSettings.CombatLog = AUTOLOGGER_DEFAULT_COMBAT;
+		end
+	end
+
+	-- Register in myAddOns
+	if (false) then
+	end
+end
+
+function AutomagicalLogger_Set()
+end
+
+AutomagicalLogger_Start();
---
+++ localization.lua
@@ -0,0 +1,67 @@
+local clientLanguage = GetLocale();
+
+if(clientLanguage=="frFR") then
+	-- French - Français
+	AUTOLOGGER_LOG_CHAT="Log chat";
+	AUTOLOGGER_LOG_COMBAT="Log combat";
+	AUTOLOGGER_OPTIONS="Automagical Logger Options";
+	AUTOLOGGER_HELP = {};
+	AUTOLOGGER_HELP[1] = "Help Page1 line1\nline2\nline3...";
+	AUTOLOGGER_HELP[2] = "Help Page2 line1\nline2\nline3...";
+
+	AUTOLOGGER_COMBAT_ALREADY_STARTED="Combat is already being logged";
+	AUTOLOGGER_COMBAT_STARTING="Combat is not being logged - starting it!";
+	AUTOLOGGER_COMBAT_STARTED="Combat is now being logged to Logs\\WOWCombatLog.txt";
+
+	AUTOLOGGER_CHAT_ALREADY_STARTED="Chat is already being logged";
+	AUTOLOGGER_CHAT_STARTING="Chat is not being logged - starting it!";
+	AUTOLOGGER_CHAT_STARTED="Chat is now being logged to Logs\\WOWChatLog.txt";
+elseif(clientLanguage=="deDE") then
+	-- German - Deutsch
+	AUTOLOGGER_LOG_CHAT="Log chat";
+	AUTOLOGGER_LOG_COMBAT="Log combat";
+	AUTOLOGGER_OPTIONS="Automagical Logger Options";
+	AUTOLOGGER_HELP = {};
+	AUTOLOGGER_HELP[1] = "Help Page1 line1\nline2\nline3...";
+	AUTOLOGGER_HELP[2] = "Help Page2 line1\nline2\nline3...";
+
+	AUTOLOGGER_COMBAT_ALREADY_STARTED="Combat is already being logged";
+	AUTOLOGGER_COMBAT_STARTING="Combat is not being logged - starting it!";
+	AUTOLOGGER_COMBAT_STARTED="Combat is now being logged to Logs\\WOWCombatLog.txt";
+
+	AUTOLOGGER_CHAT_ALREADY_STARTED="Chat is already being logged";
+	AUTOLOGGER_CHAT_STARTING="Chat is not being logged - starting it!";
+	AUTOLOGGER_CHAT_STARTED="Chat is now being logged to Logs\\WOWChatLog.txt";
+elseif(clientLanguage=="esES") then
+	-- Spanish - Español
+	AUTOLOGGER_LOG_CHAT="Log chat";
+	AUTOLOGGER_LOG_COMBAT="Log combat";
+	AUTOLOGGER_OPTIONS="Automagical Logger Options";
+	AUTOLOGGER_HELP = {};
+	AUTOLOGGER_HELP[1] = "Help Page1 line1\nline2\nline3...";
+	AUTOLOGGER_HELP[2] = "Help Page2 line1\nline2\nline3...";
+
+	AUTOLOGGER_COMBAT_ALREADY_STARTED="Combat is already being logged";
+	AUTOLOGGER_COMBAT_STARTING="Combat is not being logged - starting it!";
+	AUTOLOGGER_COMBAT_STARTED="Combat is now being logged to Logs\\WOWCombatLog.txt";
+
+	AUTOLOGGER_CHAT_ALREADY_STARTED="Chat is already being logged";
+	AUTOLOGGER_CHAT_STARTING="Chat is not being logged - starting it!";
+	AUTOLOGGER_CHAT_STARTED="Chat is now being logged to Logs\\WOWChatLog.txt";
+else
+	-- English
+	AUTOLOGGER_LOG_CHAT="Log chat";
+	AUTOLOGGER_LOG_COMBAT="Log combat";
+	AUTOLOGGER_OPTIONS="Automagical Logger Options";
+	AUTOLOGGER_HELP = {};
+	AUTOLOGGER_HELP[1] = "Help Page1 line1\nline2\nline3...";
+	AUTOLOGGER_HELP[2] = "Help Page2 line1\nline2\nline3...";
+
+	AUTOLOGGER_COMBAT_ALREADY_STARTED="Combat is already being logged";
+	AUTOLOGGER_COMBAT_STARTING="Combat is not being logged - starting it!";
+	AUTOLOGGER_COMBAT_STARTED="Combat is now being logged to Logs\\WOWCombatLog.txt";
+
+	AUTOLOGGER_CHAT_ALREADY_STARTED="Chat is already being logged";
+	AUTOLOGGER_CHAT_STARTING="Chat is not being logged - starting it!";
+	AUTOLOGGER_CHAT_STARTED="Chat is now being logged to Logs\\WOWChatLog.txt";
+end
