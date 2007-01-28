9f21e2cef4e6095dae8755d92204d0f863d672b9
Maighstir <maighstir@ops-area.net>
2007-01-28 15:15:32 +0100

    First version of AutomagicalLogger, a simple automatic log starter for World of Warcraft

---
+++ AutomagicalLogger.lua
@@ -0,0 +1,16 @@
+function AutomagicalLogger() 
+	if (LoggingChat()) then
+		DEFAULT_CHAT_FRAME:AddMessage("Chat is already being logged");
+	else
+		DEFAULT_CHAT_FRAME:AddMessage("Chat is not being logged - starting it!");  
+		LoggingChat(1);
+		DEFAULT_CHAT_FRAME:AddMessage("Chat is now being logged to Logs\\WOWChatLog.txt");
+	end 
+--	if (LoggingCombat()) then
+--		DEFAULT_CHAT_FRAME:AddMessage("Combat is already being logged");
+--	else
+--		DEFAULT_CHAT_FRAME:AddMessage("Combat is not being logged - starting it!");  
+--		LoggingCombat(1);
+--		DEFAULT_CHAT_FRAME:AddMessage("Combat is now being logged to Logs\\WOWChatLog.txt");
+--	end	
+end 
