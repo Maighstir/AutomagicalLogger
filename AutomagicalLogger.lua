aa193f5ddf2da63e131b67cd065d384b30caf031
Maighstir <maighstir@ops-area.net>
2007-01-28 17:20:47 +0100

    I don't actually need that xml file.
    It's used for describing the GUI, which I haven't made yet.

--- AutomagicalLogger.lua
+++ AutomagicalLogger.lua
@@ -1,11 +1,15 @@
-function AutomagicalLogger() 
+function AutomagicalLogger()
+--Enable/disable the logs by removing/adding the "--" in the corresponding if...else...end statements
+--(By default, combat log is disabled and chat log is enabled)
+-----CHAT LOG----- 
 	if (LoggingChat()) then
 		DEFAULT_CHAT_FRAME:AddMessage("Chat is already being logged");
 	else
 		DEFAULT_CHAT_FRAME:AddMessage("Chat is not being logged - starting it!");  
 		LoggingChat(1);
 		DEFAULT_CHAT_FRAME:AddMessage("Chat is now being logged to Logs\\WOWChatLog.txt");
-	end 
+	end
+-----COMBAT LOG-----
 --	if (LoggingCombat()) then
 --		DEFAULT_CHAT_FRAME:AddMessage("Combat is already being logged");
 --	else
@@ -14,3 +18,5 @@ function AutomagicalLogger()
 --		DEFAULT_CHAT_FRAME:AddMessage("Combat is now being logged to Logs\\WOWChatLog.txt");
 --	end	
 end 
+
+AutomagicalLogger();
