0fd603d863125da02b0d8d3660fda72ec579e944
Maighstir <maighstir@ops-area.net>
2009-04-24 01:52:12 +0200

    Keeping up with WoW versions
    Added the interface version to AutoLogger's, in order to
    separate the same version across WoW updates
    
    Apparent whitespace changes again, dammit.

--- AutomagicalLogger.lua
+++ AutomagicalLogger.lua
@@ -1,6 +1,7 @@
 function AutomagicalLogger()
 --Enable/disable the logs by removing/adding the "--" in the corresponding if...else...end statements
 --(By default, combat log is disabled and chat log is enabled)
+--GUI setting may be added at a later date
 -----CHAT LOG----- 
 	if (LoggingChat()) then
 		DEFAULT_CHAT_FRAME:AddMessage("Chat is already being logged");
@@ -16,7 +17,7 @@ function AutomagicalLogger()
 --		DEFAULT_CHAT_FRAME:AddMessage("Combat is not being logged - starting it!");  
 --		LoggingCombat(1);
 --		DEFAULT_CHAT_FRAME:AddMessage("Combat is now being logged to Logs\\WOWChatLog.txt");
---	end	
+--	end
 end 
 
 AutomagicalLogger();
